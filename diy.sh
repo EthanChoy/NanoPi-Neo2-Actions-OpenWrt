cab839f50d61eb31f50037aa828de22d212a8357
EthanChoy <42546559+EthanChoy@users.noreply.github.com>
2020-03-14 18:05:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,3 +10,5 @@ git clone https://github.com/happyzhang1995/openwrt-adguardhome.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone -b 19.07_stable https://github.com/jerrykuku/luci-theme-argon.git
 sed -i 's/192.168.1.1/192.168.2.22/g' package/base-files/files/bin/config_generate
+./scripts/feeds update -a
+./scripts/feeds install -a

e3aca0fb7da6b1c9700a5b70a8e38f8de26874a8
EthanChoy <42546559+EthanChoy@users.noreply.github.com>
2020-03-14 18:35:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,9 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-git clone https://github.com/happyzhang1995/openwrt-adguardhome.git
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-git clone -b 19.07_stable https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/happyzhang1995/openwrt-adguardhome.git package/diy/openwrt-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
+#git clone -b 19.07_stable https://github.com/jerrykuku/luci-theme-argon.git package/diy/
 sed -i 's/192.168.1.1/192.168.2.22/g' package/base-files/files/bin/config_generate
 ./scripts/feeds update -a
 ./scripts/feeds install -a
